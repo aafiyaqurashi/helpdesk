2fea6a9f32aea9283744f3462ab88d54fc4067ee
Aafiya Qureshi <aafiyaqurashi826@gmail.com>
2026-09-11 09:46:45 +0530

    Check MongoDB environment

--- app.js
+++ app.js
@@ -181,6 +181,7 @@ app.post("/admin/ticket/:id/delete",isAdmin, async (req, res) => {
 
     res.redirect("/admin");
 });
+console.log("MONGODB_URI exists:", !!process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("MongoDB connected");
